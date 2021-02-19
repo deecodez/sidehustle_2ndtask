40e1fdccbd246ea8d167a38341527e843d1970c2
deecodez <obarodayo@gmail.com>
2021-02-19 20:43:25 +0100

    second task

---
+++ second_task.dart
@@ -0,0 +1,13 @@
+void main() {
+  fullname(String firstname, String lastname) {
+    print(firstname + " " + lastname);
+  }
+
+  fullname('obaro', 'oludayo');
+
+  var city = ['Tokyo', 'Lagos', 'Ogun', 'London', 'Paris'];
+  for (var i = 0; i < city.length; i++) {
+    print(city[i]);
+  }
+  ;
+}
