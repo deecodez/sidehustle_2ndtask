89a3d283bc8e00aca860803a2740b01b5efed62a
deecodez <obarodayo@gmail.com>
2021-02-20 21:13:46 +0100

    4th

---
+++ task4.dart
@@ -0,0 +1,34 @@
+// It is New Year's Day and people are in line for the Wonderland rollercoaster ride.
+//Each person wears a sticker indicating their initial position in the queue from 1 to n .
+//Any person can bribe the person directly in front of them to swap positions, but they still wear their original
+//sticker. One person can bribe at most two others. Determine the minimum number of bribes that took place to get
+//to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+//Example List people = [1,2,3,5,4,6,7,8] If person 5 bribes person 4 , the queue will look like this: 1,2,3,4,5,6,7,8 .
+// Only 1 bribe is required. Print 1. List people = [4,1,2,3] Person 4 had to bribe 3 people to get to the current
+//position. Print Too chaotic. Function Description Write a function minimumBribes.
+//Returns No value is returned. Print the minimum number of bribes necessary or Too chaotic if someone has bribed
+//more than 2 people.
+
+void main() {
+  minimumBribes(queue) {
+    bool chaotic = false;
+    var bribes = 0;
+    for (var i = 0; i < queue.length; i++) {
+      if (queue[i] - (i + 1) > 2) {
+        chaotic = true;
+      }
+      for (var j = 0; j < i; j++) {
+        if (queue[j] > queue[i]) {
+          bribes++;
+        }
+      }
+    }
+    if (chaotic == true) {
+      print('Too chaotic');
+    } else {
+      print(bribes);
+    }
+  }
+
+  minimumBribes(1);
+}
