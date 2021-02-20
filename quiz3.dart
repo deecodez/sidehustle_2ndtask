6fcc6e9097914f4dda4a0de3e50020e0fac0d967
deecodez <obarodayo@gmail.com>
2021-02-20 20:49:03 +0100

    3rd quiz

---
+++ quiz3.dart
@@ -0,0 +1,17 @@
+// A class is a blueprint for creating objects.
+// A class encapsulates data for the object.
+// Class is a wrapper that binds/encapsulates the data and methods together
+
+// Write a class blueprint that have a field of firstname, lastname and level
+//with methods/functions of getUserLevel and getFullname then have a constructor that can be called
+//every time you instantiated your class
+
+void main() {}
+
+class GetUserLevel {
+  String firstname;
+  String lastname;
+  String level;
+
+  GetUserLevel(this.firstname, this.lastname, this.level);
+}
